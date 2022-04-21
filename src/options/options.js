d9f050118dec3199d0f2ab4fe09669d7e2958d1f
asbillings07 <superswiftdeveloper@gmail.com>
2022-04-20 23:32:43 -0400

    add check all feature in options

--- src/options/options.js
+++ src/options/options.js
@@ -1,4 +1,4 @@
-import { setItem, getItem, createNotification, hasNodeRenderedBefore } from '../utils/helperFunctions.js'
+import { setItem, getItem, createNotification, hasNodeRenderedBefore, grabItem } from '../utils/helperFunctions.js'
 
 
 const getLabelName = (cssName) =>
@@ -9,14 +9,16 @@ const getLabelName = (cssName) =>
   fontWeight: 'Font Weight',
   fontSize: 'Font Size',
   imageSource: 'Image Source',
-  backgroundImage: 'Background Image'
+  backgroundImage: 'Background Image',
+  checkAll: 'Check All?'
 }[cssName])
 
 const anchor = document.getElementById('anchor')
+const highlightAnchor = document.getElementById('anchor-highlight')
 
 let settings = []
 
-function regClick(e) {
+function regClick (e) {
   const checked = e.target.checked
   const settingId = e.target.id
   if (checked) {
@@ -31,47 +33,79 @@ function regClick(e) {
     }
   }
 }
-
-function saveSettings(e) {
-  setItem({ cssGetters: settings }, () => {
-    createNotification({ title: 'Settings Saved Successfully', message: 'The settings have been updated and will take effect the next time you use the extension.' })
+const toggle = (source, boolean) => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  checkboxes.forEach(checkBox => {
+    if (checkBox !== source) {
+      checkBox.checked = boolean
+    }
   })
+}
+function toggleCheckAll (source) {
+  if (source.target.checked) {
+    toggle(source, true)
+  } else {
+    toggle(source, false)
+  }
+}
+
+function saveSettings () {
+  console.log('SETTINGS', settings)
+  // setItem({ cssGetters: settings }, () => {
+  //   createNotification({ title: 'Settings Saved Successfully', message: 'The settings have been updated and will take effect the next time you use the extension.' })
+  // })
 }
 
 const saveButton = document.getElementById('save')
-const options = ['backgroundColor', 'color', 'fontFamily', 'fontSize', 'fontWeight', 'imageSource', 'backgroundImage'];
+const radioOptions = ['blue', 'yellow', 'red', 'orange', 'purple']
+const cssOptions = ['backgroundColor', 'color', 'fontFamily', 'fontSize', 'fontWeight', 'imageSource', 'backgroundImage', 'checkAll'];
 
-options.forEach(el => {
-  createCheckbox(el, getLabelName(el))
-})
+cssOptions.forEach(el => createInput(el, getLabelName(el), 'checkbox', anchor))
 
-getItem('cssGetters', ({ cssGetters }) => {
-  settings = cssGetters
-  settings.forEach(setting => {
-    document.getElementById(setting).checked = true
-  })
+radioOptions.forEach((color, i) => createInput(i, color, 'radio', highlightAnchor))
+
+settings = await grabItem('cssGetters')
+settings.forEach(setting => {
+  document.getElementById(getLabelName(setting)).checked = true
 })
 
 saveButton.addEventListener('click', saveSettings)
 
-function createCheckbox(id, labelName) {
+function createInput (id, labelName, type, rootEl) {
   if (!hasNodeRenderedBefore(`${labelName} - ${id}`)) {
-
     const div = document.createElement('div')
     div.id = `${labelName} - ${id}`
     const label = document.createElement('label')
     const input = document.createElement('input')
 
     label.className = 'label'
-    label.htmlFor = id
+    label.htmlFor = labelName
     label.textContent = labelName
-    input.type = 'checkbox'
-    input.id = id
-    input.addEventListener('change', regClick)
+
+    input.type = type
+
+    if (type == 'checkbox') {
+      input.name = 'style'
+
+      if (labelName !== 'Check All?') {
+        input.addEventListener('change', regClick)
+      } else {
+        input.addEventListener('change', toggleCheckAll)
+      }
+
+    } else {
+      input.name = 'color'
+      div.className = 'radio-option'
+    }
+
+    input.value = labelName
+    input.id = labelName
+
+
 
     div.appendChild(label)
     div.appendChild(input)
-    anchor.appendChild(div)
+    rootEl.append(div)
   }
 }
 
